01d6dd474db485be93c0fc5afd5e080abf818932
Viktor-Rodimin <mservis77@gmail.com>
2023-05-11 21:41:20 +0300

    exercise 10

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-let user = {
-    name: "John",
-    age: 30,
-    isAmin: true
-};
-user["City of Residence"] = "Moscow";
-user.age = 40;
-delete user["City of Residence"];
-let info = prompt("Какую информацию хотите узнать о пользователе?");
-alert(user[info]);
+let name = prompt("Введите ваше имя:");
+alert()
+alert(Привет, ${name}"!");
