8cf59628fecd8ef1178779f1c3102323b097820e
Viktor-Rodimin <mservis77@gmail.com>
2023-05-11 19:16:27 +0300

    exercise 5

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
-let a = 10;
-let b = 2;
-alert("Сумма: " + (a + b ));
-alert("Разность: " + (a - b));
-alert("Произведение: " + (a * b));
-alert("Частное: " + (a / b));
+let result = Math.pow(2, 5);
+alert(result);
