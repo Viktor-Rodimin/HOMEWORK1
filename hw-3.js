6a8710989e9d176fa9290dbc4a4b72f17cf5eb1a
Viktor-Rodimin <mservis77@gmail.com>
2023-05-16 23:46:05 +0300

    задание 4

--- hw-3.js
+++ hw-3.js
@@ -16,3 +16,18 @@ else {
     console.log('Неверно');
 }
 // задание 2
+
+let d = 50;
+let e = 120;
+if (d > 100 || e > 100) {
+    console.log ('Верно');
+}
+else{
+    console.log('Неверно');
+}
+// задание 3
+
+let o = '2';
+let p = '3';
+alert(parseInt(o) + parseInt(b));
+// задание 4
