6562bb697ff3538813746477a4fa3124d2539a76
Viktor-Rodimin <mservis77@gmail.com>
2023-05-25 22:05:51 +0300

    Задание 1

---
+++ hw-5/hw-5.js
@@ -0,0 +1,12 @@
+function getMinNumber(a, b) {
+    if (a < b) {
+      return a;
+    } else {
+      return b;
+    }
+  }
+  
+  console.log(getMinNumber(8, 4)); 
+  console.log(getMinNumber(6, 6));
+//   Задание 1
+
