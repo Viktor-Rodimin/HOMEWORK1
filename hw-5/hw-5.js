d2be3f05f15fef0d645d36b89161c5bdeca60a0a
Viktor-Rodimin <mservis77@gmail.com>
2023-05-25 23:42:41 +0300

    Задание 8

--- hw-5/hw-5.js
+++ hw-5/hw-5.js
@@ -126,3 +126,24 @@ let circle1 = {
   console.log(circle2.getArea()); 
   console.log(circle2.getPerimeter()); 
 //   Задание 7
+
+function getSeason(month) {
+    if (month < 1 || month > 12) {
+      return "Такого месяца нет";
+    } else if (month >= 3 && month <= 5) {
+      return "Весна";
+    } else if (month >= 6 && month <= 8) {
+      return "Лето";
+    } else if (month >= 9 && month <= 11) {
+      return "Осень";
+    } else {
+      return "Зима";
+    }
+  }
+  
+  console.log(getSeason(3)); 
+  console.log(getSeason(6)); 
+  console.log(getSeason(9)); 
+  console.log(getSeason(12)); 
+  console.log(getSeason(13));
+//   Задание 8
