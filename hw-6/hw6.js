c87c1f760824b962b94b1030c9dcbf0feb1abf99
Viktor-Rodimin <mservis77@gmail.com>
2023-05-26 17:02:03 +0300

    Задание 5

--- hw-6/hw6.js
+++ hw-6/hw6.js
@@ -12,3 +12,18 @@ let arr = [1, 5, 4, 10, 0, 3];
 let position = arr.indexOf(4);
 console.log(position);
 // задание 2
+
+let arr = [1, 3, 5, 10, 20];
+let result = arr.join(' ');
+console.log(result);
+// Задание 3
+
+
+
+
+// Задание 4
+
+let arr = [1, 1, 1];
+arr.push(2, 2, 2);
+console.log(arr);
+// Задание 5
