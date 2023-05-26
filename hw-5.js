c81d428e34324379bc9bf66f0d3204154390b48c
Viktor-Rodimin <mservis77@gmail.com>
2023-05-26 16:37:33 +0300

    Задание 1

---
+++ hw-5.js
@@ -0,0 +1,149 @@
+function getMinNumber(a, b) {
+
+    if (a < b) {
+      return a;
+    } else {
+      return b;
+    }
+
+  }
+  
+  console.log(getMinNumber(8, 4)); 
+  console.log(getMinNumber(6, 6));
+//   Задание 1
+
+function checkEvenNunb(num) {
+
+    if (num % 2 === 0) {
+      return 'Число четное';
+    } else {
+      return 'Число нечетное';
+    }
+
+  }
+  
+  console.log(checkEvenNunb(4)); 
+  console.log(checkEvenNunb(7)); 
+//   Задание 2
+
+
+function printSquare(num) {
+
+    console.log(num * num);
+
+  }
+  
+  printSquare(4); 
+  printSquare(7);
+//   Задание 3.1
+
+function outputEvenNumb(num) {
+
+    if (num % 2 === 0) {
+      return 'Число четное';
+    } else {
+      return 'Число нечетное';
+    }
+
+  }
+  
+  console.log(outputEvenNumb(4)); 
+  console.log(outputEvenNumb(7));
+// Задание  3.2
+
+function greetUser() {
+
+    const age = parseInt(prompt("Сколько вам лет?"));
+  
+    if (age < 0) {
+      console.log("Вы ввели неправильное значение");
+    } else if (age >= 0 && age <= 12) {
+      console.log("Привет, друг!");
+    } else {
+      console.log("Добро пожаловать!");
+    }
+
+  }
+  
+  greetUser();
+//   Задание 4
+
+function multiplyNumbers(num1, num2) {
+
+    if (isNaN(num1) || isNaN(num2)) {
+      return 'Одно или оба значения не являются числом';
+    } else {
+      return num1 * num2;
+    }
+
+  }
+  
+  console.log(multiplyNumbers(4, 5));
+  console.log(multiplyNumbers('Привет', 5));
+//   Задание 5
+
+function cubeNumber() {
+
+    let num = prompt('Введите число:');
+    if (isNaN(num)) {
+      return 'Переданный параметр не является числом';
+    } else {
+      let result = num * num * num;
+      return `${num} 'в кубе равняется' ${result}`;
+    }
+
+  }
+  
+  console.log(cubeNumber());
+//   Задание 6
+
+let circle1 = {
+
+    radius: 5,
+    getArea: function() {
+      return Math.PI * this.radius ** 2;
+    },
+    getPerimeter: function() {
+      return 2 * Math.PI * this.radius;
+    }
+
+  };
+  
+  let circle2 = {
+
+    radius: 10,
+    getArea: function() {
+      return Math.PI * this.radius ** 2;
+    },
+    getPerimeter: function() {
+      return 2 * Math.PI * this.radius;
+    }
+
+  };
+  
+  console.log(circle1.getArea()); 
+  console.log(circle1.getPerimeter()); 
+  console.log(circle2.getArea()); 
+  console.log(circle2.getPerimeter()); 
+//   Задание 7
+
+function getSeason(month) {
+    if (month < 1 || month > 12) {
+      return "Такого месяца нет";
+    } else if (month >= 3 && month <= 5) {
+      return "Весна";
+    } else if (month >= 6 && month <= 8) {
+      return "Лето";
+    } else if (month >= 9 && month <= 11) {
+      return "Осень";
+    } else {
+      return "Зима";
+    }
+  }
+  
+  console.log(getSeason(3)); 
+  console.log(getSeason(6)); 
+  console.log(getSeason(9)); 
+  console.log(getSeason(12)); 
+  console.log(getSeason(13));
+//   Задание 8
---
+++ hw-6/hw6.js
@@ -0,0 +1,9 @@
+let arr = [1, 5, 4, 10, 0, 3];
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] === 10) {
+    break;
+  }
+  console.log(arr[i]);
+}
+// Задание 1
