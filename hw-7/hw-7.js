2745a73033750004ca108a8f39cd045a69cc141c
Viktor-Rodimin <mservis77@gmail.com>
2023-06-01 13:04:58 +0300

    Задание 3.1-3.3

--- hw-7/hw-7.js
+++ hw-7/hw-7.js
@@ -7,9 +7,20 @@ function searchStart(arr, prefix) {
 
 return arr.filter((s) => s.toLowerCase().startsWith(prefix.toLowerCase()));
   }
-  
+
 console.log(searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко'));
 console.log(searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру')); 
 console.log(searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино'));
 // Задание 2
 
+let num = 32.58884;
+
+let floorNum = Math.floor(num);
+console.log(floorNum);
+// Задание 3.1
+let ceilNum = Math.ceil(num);
+console.log(ceilNum);
+// Задание 3.2
+let roundNum = Math.round(num);
+console.log(roundNum);
+// Задание 3.3
