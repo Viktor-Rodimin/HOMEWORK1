1a097e34d6351701f7d70357919118b0fc82b7d7
Viktor-Rodimin <mservis77@gmail.com>
2023-06-01 12:59:04 +0300

    Задание 2

--- hw-7/hw-7.js
+++ hw-7/hw-7.js
@@ -1,3 +1,15 @@
 let line = "js";
 console.log(line.toUpperCase());
 // Задание 1
+
+
+function searchStart(arr, prefix) {
+
+return arr.filter((s) => s.toLowerCase().startsWith(prefix.toLowerCase()));
+  }
+  
+console.log(searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко'));
+console.log(searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру')); 
+console.log(searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино'));
+// Задание 2
+
