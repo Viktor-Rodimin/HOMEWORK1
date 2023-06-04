72a4ee6e920b18f1643da9f43bac4eae14da9e03
Viktor-Rodimin <mservis77@gmail.com>
2023-06-04 12:06:53 +0300

    Задание 2

--- hw-8/hw-8.js
+++ hw-8/hw-8.js
@@ -10,3 +10,23 @@ const people = [
  console.log(people);
 //  Задание 1
 
+function isPositive(num) {
+    return num > 0;
+  }
+  
+  function isMale(person) {
+    return person.gender === 'male';
+  }
+  
+  console.log(filter([3, -4, 1, 9], isPositive));
+  
+  const people = [
+     {name: 'Глеб', gender: 'male'},
+     {name: 'Анна', gender: 'female'},
+     {name: 'Олег', gender: 'male'},
+     {name: 'Оксана', gender: 'female'}
+  ];
+  
+  console.log(filter(people, isMale));
+//   Задание 2
+
