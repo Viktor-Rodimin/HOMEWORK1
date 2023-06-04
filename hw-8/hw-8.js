7685247de32535081661012749518707a5140b59
Viktor-Rodimin <mservis77@gmail.com>
2023-06-04 12:39:28 +0300

    Задание 5

--- hw-8/hw-8.js
+++ hw-8/hw-8.js
@@ -55,3 +55,19 @@ function delayForSecond(callback) {
 //   Задание 4
 
 
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+				if(cb) { 	cb(); }
+
+    }, 1000)
+}
+
+function sayHi (name) {
+    console.log(`Привет, ${name}!`);
+}
+
+delayForSecond(() => {
+    sayHi('Глеб');
+  });
+//   Задание 5
